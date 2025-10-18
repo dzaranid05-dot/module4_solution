3872b28071da047ec07e0ccc88dcc1ddbb6018b7
arananon <dzaranid05@gmail.com>
2025-10-18 10:22:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-(function () {
+(function() {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
 
     if (firstLetter === 'j') {
-      console.log("Good Bye " + names[i]);
+      byeSpeaker.speak(names[i]);
     } else {
-      console.log("Hello " + names[i]);
+      helloSpeaker.speak(names[i]);
     }
   }
 })();
-
